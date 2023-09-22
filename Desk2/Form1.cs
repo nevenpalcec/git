6458139cdab6db33cba7e5d5589d6b56fca55de9
Neven Palcec <neven.palcec@outlook.com>
2023-09-22 13:39:22 +0200

    desk2

---
+++ Desk2/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Desk2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version 1", "Test", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+
+}
