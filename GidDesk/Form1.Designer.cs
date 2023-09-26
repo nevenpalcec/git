f881eb38f7ddd8df89dc47ad234044bb9ad25beb
MyRent - Magdalena Igaly <122869280+myrent-magdalena@users.noreply.github.com>
2023-09-26 10:53:12 +0200

    Ticket 00001 (#5)
    * develop desk
    
    * GidDesk Added
    
    * new button
    
    ---------
    
    Co-authored-by: Neven Palcec <neven.palcec@outlook.com>

--- GidDesk/Form1.Designer.cs
+++ GidDesk/Form1.Designer.cs
@@ -33,16 +33,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(469, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(216, 111);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ticket0002";
+
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
