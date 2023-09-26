1237be8e8481d08c9921ac680f089f91f584294c
MyRent - Magdalena Igaly <122869280+myrent-magdalena@users.noreply.github.com>
2023-09-26 11:40:37 +0200

    Ticket 5 (#8)
    * develop desk
    
    * GidDesk Added
    
    * new button
    
    * Ticket0002 button
    
    * new form
    
    * Ticket0002 button
    
    * new button
    
    * new form
    
    * open form
    
    * Ticket05 - Button3
    
    ---------
    
    Co-authored-by: Neven Palcec <neven.palcec@outlook.com>

--- GidDesk/Form1.Designer.cs
+++ GidDesk/Form1.Designer.cs
@@ -29,10 +29,46 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            this.button1.Location = new System.Drawing.Point(369, 45);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ticket0002";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // button2
             // 
+            this.button2.Location = new System.Drawing.Point(39, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 90);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "forma 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 365);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
 
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -51,6 +87,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
